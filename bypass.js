a90c529d9d607d0ad66cc5019a60c6cddaef5435
Ben Hichem <77390862+benhichem@users.noreply.github.com>
2023-05-14 12:54:22 +0000

    ...

--- bypass.js
+++ bypass.js
@@ -10,10 +10,10 @@ const browser = await plugin.launch({
     executablePath: executablePath(),
 });
 const page = await browser.newPage();
-await page.setViewport({
+/* await page.setViewport({
     height: 1200,
     width: 1200,
-});
+}); */
 await page.goto(url, { timeout: 0 });
 await page.waitForSelector("body > div > div.website-logo > a > img", {
     timeout: 0,
--- bypass.ts
+++ bypass.ts
@@ -14,10 +14,10 @@ const browser = await plugin.launch({
 });
 
 const page = await browser.newPage();
-await page.setViewport({
+/*await page.setViewport({
   height: 1200,
   width: 1200,
-});
+});*/
 
 await page.goto(url, { timeout: 0 });
 await page.waitForSelector("body > div > div.website-logo > a > img", {
